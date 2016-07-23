518b91d72a00b936c4c97596282c5e66c41b93da
Ovidiu Bodea <antarexis@gmail.com>
2016-07-23 23:52:34 +0300

    Adding sound

--- client/playground.js
+++ client/playground.js
@@ -8,13 +8,21 @@ waveform = new Synth(acontext);
 maxim1 = new Maxim();
 maxim2 = new Maxim();
 maxim3 = new Maxim();
+maxim4 = new Maxim();
 player1 = maxim1.loadFile("drums1.wav");
 player1.loop
 player2 = maxim2.loadFile("bassline.wav");
 player2.loop
 player3 = maxim3.loadFile("arp.wav");
 player3.loop
+player4 = maxim4.loadFile("snaredrum1.wav");
 
+playSnareDrums = function() {
+	player4.volume(1);
+}
+stopSnareDrums = function() {
+	player4.volume(0);
+}
 playDrums = function(){
 	player1.volume(1);
 }
@@ -44,6 +52,7 @@ playAll = function() {
 	player1.play();
 	player2.play();
 	player3.play();
+	player4.play();
 }
 
 setSpeed = function(speed) {
@@ -51,5 +60,5 @@ setSpeed = function(speed) {
 	player1.speed(speed);
 	player2.speed(speed);
 	player3.speed(speed);
-
+	player4.speed(speed);
 }
